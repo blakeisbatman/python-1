4ff570a573975037a7d138226ff97f432caafff9
Blake Ashley <blakeisbatman1020>
2015-06-25 18:54:56 -0400

    save

---
+++ utils.py
@@ -0,0 +1,13 @@
+"""Blakes Utils Data Base"""
+
+import colors as c
+
+def ask(question):
+    print(c.red + question + c.reset)
+    answer = input("> " + c.base3).lower().strip()
+    print(c.reset)
+    return answer
+
+if __name__ == '__main__':
+    print(c.clear)
+    name = ask("What is your name?")
